16af66d5a05ecff8e594f89b5b9b36e45bb95e56
SHAJ <shakilahmedshaj@gmail.com>
2018-09-02 21:27:05 +0600

    Exit Alert Added

--- app/src/main/java/com/t3ch/shaj/android_country_profile/MainActivity.java
+++ app/src/main/java/com/t3ch/shaj/android_country_profile/MainActivity.java
@@ -1,10 +1,13 @@
 package com.t3ch.shaj.android_country_profile;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -51,5 +54,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
 
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertBuilder.setTitle(R.string.title_text);
+        alertBuilder.setMessage(R.string.MSG_text);
+        alertBuilder.setIcon(R.drawable.warning);
+
+        alertBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+
+            }
+        });
+
+        alertBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        alertBuilder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+
+        AlertDialog alertDialog = alertBuilder.create();
+        alertDialog.show();
     }
 }
