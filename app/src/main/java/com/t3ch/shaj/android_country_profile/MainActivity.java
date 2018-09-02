d4938f0e988fdcfd91379d8c0dec9b6c82a0bc6c
SHAJ <shakilahmedshaj@gmail.com>
2018-09-02 21:01:09 +0600

    App done in Linear Layout

--- app/src/main/java/com/t3ch/shaj/android_country_profile/MainActivity.java
+++ app/src/main/java/com/t3ch/shaj/android_country_profile/MainActivity.java
@@ -1,13 +1,55 @@
 package com.t3ch.shaj.android_country_profile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button bdButton, pakButton, inButton;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bdButton = findViewById(R.id.bdButton_ID);
+        pakButton = findViewById(R.id.pakButton_ID);
+        inButton = findViewById(R.id.inButton_ID);
+
+        bdButton.setOnClickListener(this);
+        pakButton.setOnClickListener(this);
+        inButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.bdButton_ID) {
+            intent = new Intent(MainActivity.this, ProfileContainer.class);
+            intent.putExtra("name", "bangladesh");
+            startActivity(intent);
+
+        }
+
+        if (v.getId() == R.id.pakButton_ID) {
+            intent = new Intent(MainActivity.this, ProfileContainer.class);
+            intent.putExtra("name", "pakistan");
+            startActivity(intent);
+
+        }
+
+        if (v.getId() == R.id.inButton_ID) {
+            intent = new Intent(MainActivity.this, ProfileContainer.class);
+            intent.putExtra("name", "india");
+            startActivity(intent);
+
+        }
+
     }
 }
--- app/src/main/java/com/t3ch/shaj/android_country_profile/ProfileContainer.java
+++ app/src/main/java/com/t3ch/shaj/android_country_profile/ProfileContainer.java
@@ -2,12 +2,49 @@ package com.t3ch.shaj.android_country_profile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileContainer extends AppCompatActivity {
+    private ImageView imageView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_container);
+
+        imageView = findViewById(R.id.IV_id);
+        textView = findViewById(R.id.Des_ID);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+
+            String countryName = bundle.getString("name");
+
+            showDetails(countryName);
+        }
+
+
+    }
+
+    void showDetails(String countryName) {
+
+        if(countryName.equals("bangladesh"))
+        {
+            imageView.setImageResource(R.drawable.bd);
+            textView.setText(R.string.bd_des);
+        }
+        if(countryName.equals("pakistan"))
+        {
+            imageView.setImageResource(R.drawable.pak);
+            textView.setText(R.string.pak_des);
+        }
+        if(countryName.equals("india"))
+        {
+            imageView.setImageResource(R.drawable.in);
+            textView.setText(R.string.in_des);
+        }
+
     }
 }
